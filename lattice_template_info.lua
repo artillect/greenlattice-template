343e9ebc03e89cacb391c4439a04faaef5059c65
artillect <oreo4455@gmail.com>
2023-07-22 02:46:29 -0700

    Add files

---
+++ lattice_template_info.lua
@@ -0,0 +1,16 @@
+{
+    "contact": "https://discord.gg/YzZfdedyT4",
+    "notifications": "",
+    "templates": [
+      {
+        "name": "Green Lattice",
+        "sources": [
+          "https://files.catbox.moe/5axw1j.png"
+        ],
+        "x": 1500,
+        "y": 453
+      }
+    ],
+    "whitelist": [],
+    "blacklist": []
+  }
