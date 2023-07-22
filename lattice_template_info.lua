ea653c91efc2172affd653f2d61daba5220172f3
Riley <oreo4455@gmail.com>
2023-07-22 16:52:57 -0700

    Update lattice_template_info.lua

--- lattice_template_info.lua
+++ lattice_template_info.lua
@@ -7,8 +7,8 @@
         "sources": [
           "https://github.com/artillect/greenlattice-template/blob/main/lattice_template.png?raw=true"
         ],
-        "x": 1500,
-        "y": 453
+        "x": 1200,
+        "y": 170
       }
     ],
     "whitelist": [],
