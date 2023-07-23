4bb6eb2cda7e38ac05b5b59d9ac8615b3ff664ba
CedarBehnke <38959557+CedarBehnke@users.noreply.github.com>
2023-07-23 11:34:28 -0700

    Update lattice_template_info.lua

--- lattice_template_info.lua
+++ lattice_template_info.lua
@@ -7,7 +7,7 @@
         "sources": [
           "https://github.com/artillect/greenlattice-template/blob/main/lattice_template.png?raw=true"
         ],
-        "x": 1395,
+        "x": 1177,
         "y": 93
       }
     ],
